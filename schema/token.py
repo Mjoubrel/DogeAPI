639f8375ca773d165f5f7540de9b5ad86316f8ed
Mjoubrel <51671029+Mjoubrel@users.noreply.github.com>
2021-08-23 01:00:18 +0200

    Correct token.py

--- schema/token.py
+++ schema/token.py
@@ -2,7 +2,7 @@
 
 from typing import Optional
 from datetime import timedelta, datetime
-from jose import jwt, JWTError
+from jose import JWT as jwt, JWTError
 from schema.schemas import TokenData
 from decouple import config
 
@@ -20,12 +20,12 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
         expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return JWT.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 def verify_token(token: str, credentials_exception):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        payload = JWT.decode(token, SECRET_KEY, algorithms=ALGORITHM)
         email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
