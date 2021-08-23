6818b2519c7c7de4d7088403889ecea1c4adbe4a
Mjoubrel <51671029+Mjoubrel@users.noreply.github.com>
2021-08-24 00:09:34 +0200

    Update database URL

--- database/configuration.py
+++ database/configuration.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from decouple import config
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://localhost/test_api'
+SQLALCHEMY_DATABASE_URL = 'postgres://qciwzghywyhpmr:f36a6ae7bf3941432e03de03f1bf64983d0996403e170e84991174583b43c2c1@ec2-3-248-103-75.eu-west-1.compute.amazonaws.com:5432/dfjnvdcki6tk8k'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
                        "check_same_thread": False})
 
